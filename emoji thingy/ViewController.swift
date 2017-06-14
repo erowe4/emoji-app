5008d502f15e0b7fb5b04f67174241fd7c0a0e40
evan rowe <xoogers@gmail.com>
2017-06-14 16:13:09 -0400

    added 10 Hello Theres

--- emoji thingy/ViewController.swift
+++ emoji thingy/ViewController.swift
@@ -4,15 +4,30 @@
 //
 //  Created by evan rowe on 6/14/17.
 //  Copyright © 2017 evan rowe. All rights reserved.
-// 🦑🦑🦑
+// 
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
 
+    @IBOutlet weak var EmojiTableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        EmojiTableView.dataSource = self
+        EmojiTableView.delegate = self
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10
+    
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell()
+        cell.textLabel?.text = "Hello there"
+        return cell
     }
 
     override func didReceiveMemoryWarning() {
